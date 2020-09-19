66d5af3c4a7e56aeb8d3b294dfbffb4353ff623a
vapvin <vins@vins.dev>
2020-09-19 21:11:22 +0900

    Rocket Documentation

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ fn index() -> &'static str {
     "Hello World"
 }
 
+mod other {
+    #[get("/world")]
+    pub fn world() -> &'static str {
+        "Hello, world!"
+    }
+}
+
+#[get("/hello")]
+pub fn hello() -> &'static str {
+    "Hello, outside world!"
+}
+
+use other::world;
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    // error[E0425]: cannot find value `static_rocket_route_info_for_world` in this scope
+    rocket::ignite().mount("/hello", routes![hello, world]);
 }
